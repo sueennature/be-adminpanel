6598caaa828989fc4a8dd7c9fdf27411c584a102
William1419 <william19991116@gmail.com>
2024-07-10 22:04:58 +0530

    added pagination

--- src/components/Tables/roomTable.tsx
+++ src/components/Tables/roomTable.tsx
@@ -6,7 +6,6 @@ import { Edit, Trash, Eye, Plus } from 'react-feather';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 
-
 interface RoomData {
     id: number;
     name: string;
@@ -23,12 +22,13 @@ interface RoomData {
 }
 
 const RoomTable = () => {
-
     const [rooms, setRooms] = React.useState<RoomData[]>([]);
     const [nameFilter, setNameFilter] = React.useState<string>('');
-    const [idFilter, setIdFilter] = React.useState<string>('');
-    const [roomsSelection, setRoomsSelection] = React.useState<number[]>([]);;
+    const [roomsSelection, setRoomsSelection] = React.useState<number[]>([]);
+    const [currentPage, setCurrentPage] = React.useState<number>(1);
+    const [itemsPerPage, setItemsPerPage] = React.useState<number>(10);
     const router = useRouter();
+    const [idFilter, setIdFilter] = React.useState<string>('');
 
     React.useEffect(() => {
         setRooms(roomData);
@@ -39,122 +39,155 @@ const RoomTable = () => {
         String(room.id).toLowerCase().includes(idFilter.toLowerCase())
     );
 
-    const handleSelectAll = (e:React.ChangeEvent<HTMLInputElement>)=>{
-        if(e.target.checked){
-            const allRoomIds = filteredRooms.map(room => room.id);
-            setRoomsSelection(allRoomIds)
-        }else{
-            setRoomsSelection([]);
-        }
-    }
+    const indexOfLastItem = currentPage * itemsPerPage;
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+    const currentItems = filteredRooms.slice(indexOfFirstItem, indexOfLastItem);
 
-    const handleCheckboxChange = (e: React.ChangeEvent<HTMLInputElement>, roomId: number) => {
+    const handleSelectAll = (e: React.ChangeEvent<HTMLInputElement>) => {
         if (e.target.checked) {
-            setRoomsSelection(prevSelected => [...prevSelected, roomId]);
+            const allRoomIds = currentItems.map((room) => room.id);
+            setRoomsSelection(allRoomIds);
         } else {
-            setRoomsSelection(prevSelected => prevSelected.filter(id => id !== roomId));
+            setRoomsSelection([]);
         }
     };
 
+    const handleCheckboxChange = (e: React.ChangeEvent<HTMLInputElement>, roomId: number) => {
+        if (e.target.checked) {
+            setRoomsSelection((prevSelected) => [...prevSelected, roomId]);
+        } else {
+            setRoomsSelection((prevSelected) => prevSelected.filter((id) => id !== roomId));
+        }
+    };
 
-    const handleEditPush =(roomData: any)=>{
-        router.push(`/rooms/update/${roomData.id}`)
-    }
+    const handleEditPush = (roomData: any) => {
+        router.push(`/rooms/update/${roomData.id}`);
+    };
 
-    const handleViewPush =(roomData: any)=>{
-        router.push(`/rooms/view/${roomData.id}`)
-    }
-  return (
-    <div>
-    <div className='flex items-center justify-between mb-4'>
-        <div className='flex items-center gap-4'>
-            <input
-                type='text'
-                placeholder='Search by Name'
-                value={nameFilter}
-                onChange={(e) => setNameFilter(e.target.value)}
-                className='px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500'
-            />
-            {/* <input
-                type='text'
-                placeholder='Search by ID'
-                value={idFilter}
-                onChange={(e) => setIdFilter(e.target.value)}
-                className='px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500'
-            /> */}
-        </div>
-        <div className='text-blue-400 cursor-pointer'>
-            <Link href='/rooms/createRoom'>
-                <Plus />
-            </Link>
-        </div>
-    </div>
-         <div className='bg-white '>
-              <div className="overflow-x-auto shadow-md sm:rounded-lg">
-            <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-                    <tr>
-                        <th scope="col" className="p-4">
-                            <div className="flex items-center">
-                                <input
-                                    id="checkbox-all-search"
-                                    type="checkbox"
-                                    onChange={handleSelectAll}
-                                    checked={roomsSelection.length === filteredRooms.length}
-                                    className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                                />
-                                <label htmlFor="checkbox-all-search" className="sr-only">
-                                    checkbox
-                                </label>
-                            </div>
-                        </th>
-                        <th scope="col" className="px-6 py-3">id</th>
-                        <th scope="col" className="px-6 py-3">Name
-                        </th>
-                        <th scope="col" className="px-6 py-3">No of Room</th>
-                        <th scope="col" className="px-6 py-3">Max Adults</th>
-                        <th scope="col" className="px-6 py-3"> Max Childs</th>
-                        <th scope="col" className="px-6 py-3">Description</th>
-                        <th scope="col" className="px-6 py-3">Features</th>
-                        <th scope="col" className="px-6 py-3">Beds</th>
-                        <th scope="col" className="px-6 py-3">Size</th>
-                        <th scope="col" className="px-6 py-3">Bathrooms</th>
-                        <th scope="col" className="px-6 py-3">Images</th>
-                        <th scope="col" className="px-6 py-3">Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {filteredRooms.map((room) => (
-                        <tr
-                            key={room.id}
-                            className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600 text-black"
-                        >
-                            <td className="w-4 p-4">
-                                <div className="flex items-center">
-                                    <input
-                                        id={`checkbox-table-search-${room.id}`}
-                                        type="checkbox"
-                                        checked={roomsSelection.includes(room.id)}
-                                        onChange={(e) => handleCheckboxChange(e, room.id)}
-                                    
-                                        className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                                    />
-                                    <label htmlFor={`checkbox-table-search-${room.id}`} className="sr-only">
-                                        checkbox
-                                    </label>
-                                </div>
-                            </td>
-                            <td className="px-6 py-4">{room.id}</td>
-                            <td className="px-6 py-4">{room.name}</td>
-                            <td className="px-6 py-4">{room.noOfRoom}</td>
-                            <td className="px-6 py-4">{room.maxAdults}</td>
-                            <td className="px-6 py-4">{room.maxChilds}</td>
-                            <td className="px-6 py-4">{room.description}</td>
-                            <td className="px-6 py-4">{room.features.join(', ')}</td>
-                            <td className="px-6 py-4">{room.beds}</td>
-                            <td className="px-6 py-4">{room.size}</td>
-                            <td className="px-6 py-4">{room.bathroom}</td>
-                            <td className="px-6 py-4" style={{ minWidth: '200px' }}>
+    const handleViewPush = (roomData: any) => {
+        router.push(`/rooms/view/${roomData.id}`);
+    };
+
+    const handleItemsPerPageChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        setItemsPerPage(Number(e.target.value));
+        setCurrentPage(1); 
+    };
+
+    const nextPage = () => {
+        setCurrentPage((prev) => prev + 1);
+    };
+
+    const prevPage = () => {
+        setCurrentPage((prev) => prev - 1);
+    };
+
+    return (
+        <div>
+            <div className="flex items-center justify-between mb-4">
+                <div className="flex items-center gap-4">
+                    <input
+                        type="text"
+                        placeholder="Search by Name"
+                        value={nameFilter}
+                        onChange={(e) => setNameFilter(e.target.value)}
+                        className="px-4 py-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    />
+                </div>
+                <div className="text-blue-400 cursor-pointer">
+                    <Link href="/rooms/createRoom">
+                        <Plus />
+                    </Link>
+                </div>
+            </div>
+        
+            <div className="bg-white">
+                <div className="overflow-x-auto shadow-md sm:rounded-lg">
+                    <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                            <tr>
+                                <th scope="col" className="p-4">
+                                    <div className="flex items-center">
+                                        <input
+                                            id="checkbox-all-search"
+                                            type="checkbox"
+                                            onChange={handleSelectAll}
+                                            checked={roomsSelection.length === currentItems.length}
+                                            className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                                        />
+                                        <label htmlFor="checkbox-all-search" className="sr-only">
+                                            checkbox
+                                        </label>
+                                    </div>
+                                </th>
+                                <th scope="col" className="px-6 py-3">
+                                    id
+                                </th>
+                                <th scope="col" className="px-6 py-3">
+                                    Name
+                                </th>
+                                <th scope="col" className="px-6 py-3">
+                                    No of Room
+                                </th>
+                                <th scope="col" className="px-6 py-3">
+                                    Max Adults
+                                </th>
+                                <th scope="col" className="px-6 py-3">
+                                    Max Childs
+                                </th>
+                                <th scope="col" className="px-6 py-3">
+                                    Description
+                                </th>
+                                <th scope="col" className="px-6 py-3">
+                                    Features
+                                </th>
+                                <th scope="col" className="px-6 py-3">
+                                    Beds
+                                </th>
+                                <th scope="col" className="px-6 py-3">
+                                    Size
+                                </th>
+                                <th scope="col" className="px-6 py-3">
+                                    Bathrooms
+                                </th>
+                                <th scope="col" className="px-6 py-3">
+                                    Images
+                                </th>
+                                <th scope="col" className="px-6 py-3">
+                                    Action
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {currentItems.map((room) => (
+                                <tr
+                                    key={room.id}
+                                    className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600 text-black"
+                                >
+                                    <td className="w-4 p-4">
+                                        <div className="flex items-center">
+                                            <input
+                                                id={`checkbox-table-search-${room.id}`}
+                                                type="checkbox"
+                                                checked={roomsSelection.includes(room.id)}
+                                                onChange={(e) => handleCheckboxChange(e, room.id)}
+                                                className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                                            />
+                                            <label htmlFor={`checkbox-table-search-${room.id}`} className="sr-only">
+                                                checkbox
+                                            </label>
+                                        </div>
+                                    </td>
+                                    <td className="px-6 py-4">{room.id}</td>
+                                    <td className="px-6 py-4">{room.name}</td>
+                                    <td className="px-6 py-4">{room.noOfRoom}</td>
+                                    <td className="px-6 py-4">{room.maxAdults}</td>
+                                    <td className="px-6 py-4">{room.maxChilds}</td>
+                                    <td className="px-6 py-4">{room.description}</td>
+                                    <td className="px-6 py-4">{room.features.join(', ')}</td>
+                                    <td className="px-6 py-4">{room.beds}</td>
+                                    <td className="px-6 py-4">{room.size}</td>
+                                    <td className="px-6 py-4">{room.bathroom}</td>
+                                    <td className="px-6 py-4" style={{ minWidth: '200px' }}>
                                 <div className="flex items-center gap-2">
                                     {room.images.map((image, index) => (
                                         <div key={index} className="flex-shrink-0">
@@ -162,7 +195,7 @@ const RoomTable = () => {
                                         </div>
                                     ))}
                                 </div>
-                            </td>
+                            </td>                                    
                             <td className="px-6 py-4">
                                 <div className="flex items-center gap-4 ">
                                     <button onClick={()=>handleEditPush(room)}  className="font-medium text-blue-600 dark:text-blue-500 hover:underline">
@@ -176,15 +209,44 @@ const RoomTable = () => {
                                     </a>
                                 </div>
                             </td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+                <div className="flex justify-between mt-4 p-4">
+                <div className="flex items-center gap-4">
+                <select
+                    value={itemsPerPage}
+                    onChange={handleItemsPerPageChange}
+                    className="px-2 py-1 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                >
+                    <option value={10}>10</option>
+                    <option value={20}>20</option>
+                    <option value={30}>50</option>
+                </select>
+                <span>items per page</span>
+            </div>
+                  <div>
+                  <button
+                        onClick={prevPage}
+                        disabled={currentPage === 1}
+                        className="px-3 py-1 mr-2 bg-gray-200 dark:bg-gray-700 text-gray-600 dark:text-gray-400 rounded-md cursor-pointer"
+                    >
+                        Previous
+                    </button>
+                    <button
+                        onClick={nextPage}
+                        disabled={indexOfLastItem >= filteredRooms.length}
+                        className="px-3 py-1 bg-gray-200 dark:bg-gray-700 text-gray-600 dark:text-gray-400 rounded-md cursor-pointer"
+                    >
+                        Next
+                    </button>
+                  </div>
+                </div>
+            </div>
         </div>
-    </div>
-    </div>
-   
-  )
-}
+    );
+};
 
-export default RoomTable
+export default RoomTable;
