1b42c9a2637c3f7982f51e178debc3c4e51a1125
shaniAbeywick <shaniabeywickrama294@gmail.com>
2024-08-14 03:39:24 +0530

    added the style changes to calender

--- src/components/Calender/index.tsx
+++ src/components/Calender/index.tsx
@@ -5,6 +5,8 @@ import bookingsData from "../../components/Datatables/eventData.json";
 import roomsData from "../../components/Datatables/roomsTypes.json";
 import Cookies from "js-cookie";
 import axios from "axios";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 interface Booking {
   start: string;
@@ -17,6 +19,7 @@ interface Booking {
 interface Room {
   id: string;
   room_type: string;
+  status: string; // Added status field
 }
 
 const colors = [
@@ -52,8 +55,9 @@ const useMediaQuery = (query: string): boolean => {
 
 const Home: React.FC = () => {
   const [bookings, setBookings] = useState<Booking[]>([]);
-  const [rooms, setRooms] = useState<Room[]>([]);
-  const [startDate, setStartDate] = useState(dayjs().startOf('month'));
+const [rooms, setRooms] = useState<Room[]>([]);
+  const [startDate, setStartDate] = useState(dayjs().startOf("month"));
+  const [selectedDate, setSelectedDate] = useState<Date | null>(new Date()); //for get date from search input field
   const [showPopup, setShowPopup] = useState<boolean>(false);
   const [popupContent, setPopupContent] = useState<{
     refNo: string;
@@ -66,7 +70,7 @@ const Home: React.FC = () => {
   const popupRef = useRef<HTMLDivElement>(null);
 
   const isMobile = useMediaQuery("(max-width: 1124px)");
-  function convertToISO8601(dateStr:any) {
+  function convertToISO8601(dateStr: any) {
     // Convert the input date string to a Date object
     let date = new Date(dateStr);
 
@@ -74,70 +78,80 @@ const Home: React.FC = () => {
     let isoFormattedDateStr = date.toISOString();
 
     return isoFormattedDateStr;
-}
-  async function transformBookingData(inputArray : any) {
-    return await inputArray.map( async (booking: any) => {
-        return {
-            start: await convertToISO8601(booking.check_in),
-            end: await convertToISO8601(booking.check_out),
-            refNo: booking.booking_id,
-            personName: booking.guest_name,
-            room: booking.room_number
-        };
+  }
+  async function transformBookingData(inputArray: any) {
+    return await inputArray.map(async (booking: any) => {
+      return {
+        start: await convertToISO8601(booking.check_in),
+        end: await convertToISO8601(booking.check_out),
+        refNo: booking.booking_id,
+        personName: booking.guest_name,
+        room: booking.room_number,
+      };
     });
-}
+  }
 
-  const fetchBookings = async() =>{
-    try{
+  const fetchBookings = async () => {
+    try {
       const accessToken = Cookies.get("access_token");
-      const response = await axios.get(`${process.env.BE_URL}/calendar/data`,{
+      const response = await axios.get(`${process.env.BE_URL}/calendar/data`, {
         headers: {
           "Content-Type": "application/json",
           Authorization: `Bearer ${accessToken}`,
           "x-api-key": process.env.X_API_KEY,
         },
       });
-      
-     // console.log("response?.data",response)
-      console.log("bookingsData",)
-      console.log("roomsData",roomsData)
-      console.log("response",roomsData?.rooms )
-      console.log("response?.data?.rooms",response?.data?.rooms )
-      
-      const transformedArray = await transformBookingData(response?.data);
-      console.log("transformedArray",response?.data)
-      setBookings( bookingsData || []);
-      setRooms( roomsData?.rooms || []);
-    }catch(err){
-      console.log(err)
-    }
-  }
 
-  
+      // console.log("response?.data",response)
+      console.log("bookingsData");
+      console.log("roomsData", roomsData);
+      console.log("response", roomsData?.rooms);
+      console.log("response?.data?.rooms", response?.data?.rooms);
+
+      const transformedArray = await transformBookingData(response?.data);
+      console.log("transformedArray", response?.data);
+      setBookings(bookingsData || []);
+      setRooms(roomsData?.rooms || []);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   useEffect(() => {
-    fetchBookings()
+    fetchBookings();
   }, []);
 
   const navigate = (direction: "previous" | "next") => {
     if (isMobile) {
-      let newStartDate = direction === "previous"
-        ? startDate.subtract(7, "day")
-        : startDate.add(7, "day");
+      let newStartDate =
+        direction === "previous"
+          ? startDate.subtract(7, "day")
+          : startDate.add(7, "day");
 
       // Check if the new date is out of the current month
       const daysInMonth = dayjs(newStartDate).daysInMonth();
       if (newStartDate.date() > daysInMonth) {
-        newStartDate = newStartDate.startOf('month').add(1, 'month').startOf('month');
+        newStartDate = newStartDate
+          .startOf("month")
+          .add(1, "month")
+          .startOf("month");
       }
       setStartDate(newStartDate);
     } else {
-      const newStartDate = direction === "previous"
-        ? startDate.subtract(1, "month")
-        : startDate.add(1, "month");
-        
-      setStartDate(newStartDate.startOf('month'));
+      const newStartDate =
+        direction === "previous"
+          ? startDate.subtract(1, "month")
+          : startDate.add(1, "month");
+
+      setStartDate(newStartDate.startOf("month"));
     }
   };
+
+  const handleSearch = () => {
+    if (selectedDate) {
+      setStartDate(dayjs(selectedDate).startOf("month"));
+    }
+  }; //for input search
 
   const handleMouseEnter = (roomId: string, day: number) => {
     const booking = bookings.find(
@@ -160,8 +174,10 @@ const Home: React.FC = () => {
   };
 
   const getDaysForWeek = (date: dayjs.Dayjs): number[] => {
-    const startOfWeek = date.startOf('week');
-    return new Array(7).fill(null).map((_, index) => startOfWeek.add(index, 'day').date());
+    const startOfWeek = date.startOf("week");
+    return new Array(7)
+      .fill(null)
+      .map((_, index) => startOfWeek.add(index, "day").date());
   };
 
   const getDaysForMonth = (date: dayjs.Dayjs): number[] => {
@@ -169,10 +185,37 @@ const Home: React.FC = () => {
     return new Array(daysInMonth).fill(null).map((_, index) => index + 1);
   };
 
-  const daysToShow = isMobile ? getDaysForWeek(startDate) : getDaysForMonth(startDate);
+  const daysToShow = isMobile
+    ? getDaysForWeek(startDate)
+    : getDaysForMonth(startDate);
 
   return (
-    <div className="p-4">
+    <div className="my-8 h-full bg-white p-4">
+      <div className="mb-4 mt-2">
+        <label
+          htmlFor="monthPicker"
+          className="text-gray-700 mb-2 block font-bold"
+        >
+          Month
+        </label>
+        <div className="flex items-center">
+          <DatePicker
+            id="monthPicker"
+            selected={selectedDate}
+            onChange={(date: Date | null) => setSelectedDate(date)}
+            dateFormat="yyyy/MM/dd"
+            className="rounded-sm border p-2"
+            placeholderText="Select a date"
+            showPopperArrow={false}
+          />
+          <button
+            onClick={handleSearch}
+            className="ml-2 rounded-sm bg-blue-900 px-4 py-2 text-white shadow-sm shadow-black hover:bg-blue-600"
+          >
+            Search
+          </button>
+        </div>
+      </div>
       <div className="mb-4 flex items-center justify-between">
         <button
           className="bg-gray-200 hover:bg-gray-300 flex items-center rounded-md px-4 py-2"
@@ -186,23 +229,21 @@ const Home: React.FC = () => {
             className="ml-2"
           >
             <path
-              fill="#3B82F6"
+              fill="#2357ac"
               d="M2 10l8 8l1.4-1.4L5.8 11H18V9H5.8l5.6-5.6L10 2z"
             />
           </svg>
-          <div className="sm:block hidden"
-          >Previous</div>
+          <div className="ml-2 hidden sm:block">Previous</div>
         </button>
-        <h2 className="md:text-lg text-md font-semibold">
+        <h2 className="text-md font-semibold md:text-lg">
           {startDate.format(isMobile ? "MMMM YYYY" : "MMMM YYYY")}
         </h2>
         <button
           className="bg-gray-200 hover:bg-gray-300 flex items-center rounded-md px-4 py-2"
           onClick={() => navigate("next")}
         >
-          <div className="sm:block hidden"
-          >Next{" "}</div>
-          
+          <div className="mr-2 hidden sm:block">Next </div>
+
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="1em"
@@ -210,7 +251,7 @@ const Home: React.FC = () => {
             viewBox="0 0 20 20"
           >
             <path
-              fill="#3B82F6"
+              fill="#2357ac"
               d="M8.6 3.4L14.2 9H2v2h12.2l-5.6 5.6L10 18l8-8l-8-8z"
             />
           </svg>
@@ -220,14 +261,24 @@ const Home: React.FC = () => {
         <table className="w-full table-fixed border-collapse">
           <thead>
             <tr>
-              <th className="2xl:w-36 w-18 border px-2 py-1 2xl:text-xs text-xxs">Room Type</th>
-              <th className="2xl:w-24 w-10 border px-2 py-1 2xl:text-xs text-xxs">Room</th>
+              <th className="w-16 border px-2 py-1 text-xxs 2xl:w-30 2xl:text-xs">
+                Room Type
+              </th>
+              <th className="w-10 border px-2 py-1 text-xxs 2xl:w-24 2xl:text-xs">
+                Room
+              </th>
+              <th className="w-16 border px-2 py-1 text-xxs 2xl:w-24 2xl:text-xs">
+                Room View
+              </th>
+
               {daysToShow.map((day) => (
                 <th
                   key={day}
-                  className="border px-2 py-1 text-center 2xl:text-xs text-xxs"
+                  className="border px-2 py-1 text-center text-xxs 2xl:text-xxs"
                 >
-                  {day}
+                  <div className="flex justify-center font-bold">
+      {String(day).padStart(2, '0')}
+    </div>
                 </th>
               ))}
             </tr>
@@ -235,11 +286,28 @@ const Home: React.FC = () => {
           <tbody>
             {rooms.map((room) => (
               <tr key={room.id}>
-                <td className="border px-2 py-1 2xl:text-xs text-xxs text-center text-nowrap">
+                <td className="text-nowrap text-white border-black px-2 py-1 text-center text-xxs 2xl:text-xs bg-blue-900" style={{
+    
+    boxShadow: '0 -5px 5px -5px rgba(0, 0, 0, 0.7), 0 5px 5px -5px rgba(0, 0, 0, 0.7)',
+  }}>
                   {room.room_type}
                 </td>
-                <td className="border px-2 py-1 2xl:text-xs text-xxs text-center">
+                <td className="border-black text-white px-2 py-1 text-center text-xxs 2xl:text-xs bg-blue-900" style={{
+    
+    boxShadow: '0 -5px 5px -5px rgba(0, 0, 0, 0.7), 0 5px 5px -5px rgba(0, 0, 0, 0.7)',
+  }}>
                   {room.id}
+                </td>
+                <td className="text-nowrap text-white border-black px-2 py-1 text-center text-xxs 2xl:text-xs bg-blue-900" style={{
+    
+    boxShadow: '0 -5px 5px -5px rgba(0, 0, 0, 0.7), 0 5px 5px -5px rgba(0, 0, 0, 0.7)',
+  }}>
+                  Lake View
+                </td>
+                <td className="border-2 border-green-600 px-2 py-1 text-center text-xxs text-black 2xl:text-xs" style={{
+    boxShadow: '0 -5px 5px -5px rgba(0, 0, 0, 0.7), 0 5px 5px -5px rgba(0, 0, 0, 0.7)',
+  }}>
+
                 </td>
                 {daysToShow.map((day) => {
                   const booking = bookings.find(
@@ -247,7 +315,8 @@ const Home: React.FC = () => {
                       booking.room === room.id &&
                       dayjs(booking.start).date() <= day &&
                       dayjs(booking.end).date() >= day &&
-                      dayjs(booking.start).month() + 1 === startDate.month() + 1 &&
+                      dayjs(booking.start).month() + 1 ===
+                        startDate.month() + 1 &&
                       dayjs(booking.start).year() === startDate.year(),
                   );
 
@@ -271,20 +340,20 @@ const Home: React.FC = () => {
                         hoveredCell?.day === day && (
                           <div
                             ref={popupRef}
-                            className="border-gray-200 absolute rounded-md border bg-black p-4 shadow-md translate-y-6 -translate-x-25"
+                            className="border-gray-200 absolute -translate-x-25 translate-y-6 rounded-md border bg-black p-4 shadow-md"
                             style={{
                               top: popupRef.current?.offsetTop,
                               left: popupRef.current?.offsetLeft,
                               transform: "",
                             }}
                           >
-                            <p className="md:text-sm text-xxs font-semibold">
+                            <p className="text-xxs font-semibold md:text-sm">
                               Booking Details
                             </p>
-                            <p className="md:text-sm text-xxs">
+                            <p className="text-xxs md:text-sm">
                               Ref No: {popupContent?.refNo}
                             </p>
-                            <p className="md:text-sm text-xxs">
+                            <p className="text-xxs md:text-sm">
                               Person: {popupContent?.personName}
                             </p>
                           </div>
