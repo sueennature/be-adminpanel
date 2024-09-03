8f9e7325f1d6065d7d71b5417f67cbcfac1dc206
William-Boomer-Baker <williamboomer87@gmail.com>
2024-09-03 16:54:39 +0530

    Update to add rooms

--- src/components/Dashboard/E-commerce.tsx
+++ src/components/Dashboard/E-commerce.tsx
@@ -6,7 +6,6 @@ import { Calendar} from 'react-feather';
 import CheckAvailability from '@/components/Discounts/CheckAvailability'
 import Cookies from "js-cookie";
 import axios from "axios";
-
 const ECommerce: React.FC = () => {
   const [dashboardDataDaly, setDashboardDataDaly] = useState<any>({});
   const [dashboardDataMonthly, setDashboardDataMonthly] = useState<any>({});
@@ -23,7 +22,6 @@ const ECommerce: React.FC = () => {
           },
         });
 
-        console.log("fetchDashBoardData", response)
         setDashboardDataDaly(response?.data)
       } catch (err) {
         console.log(err)
@@ -39,7 +37,6 @@ const ECommerce: React.FC = () => {
             "x-api-key": process.env.X_API_KEY,
           },
         });
-        console.log("setDashboardDataMonthly", response)
         setDashboardDataMonthly(response?.data)
       } catch (err) {
         console.log(err)
@@ -50,36 +47,35 @@ const ECommerce: React.FC = () => {
     const intervalId = setInterval(fetchDashBoardData, 3600000);
     return () => clearInterval(intervalId);
   }, [])
-
-  const dd ={
-    "Total_Bookings": 3,
-    "Booking Status": {
-        "completed": 1,
-        "pending": 2,
-        "failed": 0
+  const chartData = {
+    Booking_Status_Count: {
+      Total_Bookings: 1,
+      Completed: 0,
+      Pending: 1,
+      Failed: 0,
+      Check_In_Count: 0,
+      Check_Out_Count: 1
     },
-    "Total Guests": 5,
-    "Guest Count": {
-        "adults": 4,
-        "children": 1,
-        "infants": 0
+    Guest_Count: {
+      Total_Guests: 9,
+      adults: 7,
+      children: 2,
+      infants: 0
     },
-    "Room Availability": {
-        "Single": 8,
-        "Double": 6,
-        "Triple": 18,
-        "Deluxe": 2,
-        "Family": 17
+    Room_Availability: {
+      Total_Available_Rooms: 24,
+      Single: 7,
+      Double: 5,
+      Triple: 16,
+      Deluxe: 2,
+      Family: 16
     },
-    "Total Available Rooms": 26,
-    "Meal Plan Count": {
-        "breakfast": 0,
-        "lunch": 2,
-        "dinner": 2
+    Meal_Plan_Count: {
+      breakfast: 0,
+      lunch: 3,
+      dinner: 3
     }
-}
-
-
+  };
   return (
     <>
       <CheckAvailability />
@@ -89,101 +85,18 @@ const ECommerce: React.FC = () => {
       </h3>
       <div className="grid grid-cols-1 gap-4 md:gap-6 lg:grid-cols-4 2xl:gap-7.5 mt-6">
       {Object.entries(dashboardDataDaly).map(([status, value] :any, key) => (
-          <Card title={status}data={value || {}} > <Calendar /></Card>      
-
+          <Card key={key} title={status}data={value || {}} > <Calendar /></Card>      
       ))}
-       
-       
-        {/* <Card title={"Booking Status"}data={dashboardDataDaly?.Room_Availability || {}} > <Calendar /></Card>      
-        <Card title={"Booking Status"}data={dashboardDataDaly?.Meal_Plan_Count || {}} > <Calendar /></Card>      
-        <Card title={"Booking Status"}data={dashboardDataDaly?.Guest_Count || {}} > <Calendar /></Card> */}
       </div>
       
-     
-      {/* <div className="grid grid-cols-1 gap-4 md:gap-6 lg:grid-cols-4 2xl:gap-7.5 mt-6">
-        <CardDataStats title={"Total Available Rooms"} total={String(dashboardDataDaly?.Total_Available_Rooms || 0)} rate={""}  >
-          <Calendar />
-        </CardDataStats>
-        <CardDataStats title={"Total Bookings"} total={String(dashboardDataDaly?.Total_Guests || 0)} rate={""}  >
-          <Calendar />
-        </CardDataStats>
-        <CardDataStats title={"Total Guests"} total={String(dashboardDataDaly?.Total_Guests || 0)} rate={""}  >
-          <Calendar />
-        </CardDataStats>
-
-        <CardDataStats title={"Completed Bookings"} total={String(dashboardDataDaly?.Booking_Status_Count?.completed || 0)} rate={""}  >
-          <Calendar />
-        </CardDataStats>
-        <CardDataStats title={"failed Bookings"} total={String(dashboardDataDaly?.Booking_Status_Count?.failed || 0)} rate={""}  >
-          <Calendar />
-        </CardDataStats>
-        <CardDataStats title={"pending Bookings"} total={String(dashboardDataDaly?.Booking_Status_Count?.pending || 0)} rate={""}  >
-          <Calendar />
-        </CardDataStats>
-
-        <CardDataStats title={"adults"} total={String(dashboardDataDaly?.Guest_Count?.adults || 0)} rate={""}  >
-          <Calendar />
-        </CardDataStats>
-        <CardDataStats title={"children"} total={String(dashboardDataDaly?.Guest_Count?.children || 0)} rate={""}  >
-          <Calendar />
-        </CardDataStats>
-        <CardDataStats title={"infants"} total={String(dashboardDataDaly?.Guest_Count?.infants || 0)} rate={""}  >
-          <Calendar />
-        </CardDataStats>
-
-        <CardDataStats title={"breakfast"} total={String(dashboardDataDaly?.Meal_Plan_Count?.breakfast || 0)} rate={""}  >
-          <Calendar />
-        </CardDataStats>
-        <CardDataStats title={"dinner"} total={String(dashboardDataDaly?.Meal_Plan_Count?.dinner || 0)} rate={""}  >
-          <Calendar />
-        </CardDataStats>
-        <CardDataStats title={"lunch"} total={String(dashboardDataDaly?.Meal_Plan_Count?.lunch || 0)} rate={""}  >
-          <Calendar />
-        </CardDataStats>
-
-        <CardDataStats title={"Deluxe"} total={String(dashboardDataDaly?.Room_Availability?.Deluxe || 0)} rate={""}  >
-          <Calendar />
-        </CardDataStats>
-        <CardDataStats title={"Double"} total={String(dashboardDataDaly?.Room_Availability?.Double || 0)} rate={""}  >
-          <Calendar />
-        </CardDataStats>
-        <CardDataStats title={"Family"} total={String(dashboardDataDaly?.Room_Availability?.Family || 0)} rate={""}  >
-          <Calendar />
-        </CardDataStats>
-        <CardDataStats title={"Single"} total={String(dashboardDataDaly?.Room_Availability?.Single || 0)} rate={""}  >
-          <Calendar />
-        </CardDataStats>
-        <CardDataStats title={"Triple"} total={String(dashboardDataDaly?.Room_Availability?.Triple || 0)} rate={""}  >
-          <Calendar />
-        </CardDataStats>
-
-      </div> */}
       <h3 className="m-2 text-2xl font-bold text-black">
         Monthly Counts
       </h3>
       <div className="grid grid-cols-1 gap-4 md:gap-6 lg:grid-cols-4 2xl:gap-7.5 mt-6">
-        <CardDataStats title={"Total Bookings"} total={String(dashboardDataMonthly?.Monthly_Total_Bookings || 0)} rate={""}  >
-          <Calendar />
-        </CardDataStats>
-        <CardDataStats title={"Out Of Service Rooms"} total={String(dashboardDataMonthly?.Out_Of_Service_Rooms || 0)} rate={""}  >
-          <Calendar />
-        </CardDataStats>
-        <CardDataStats title={"Total Rooms"} total={String(dashboardDataMonthly?.Total_Rooms || 0)} rate={""}  >
-          <Calendar />
-        </CardDataStats>
-
-        {/* Monthly_Booking_Status_Count */}
-        <CardDataStats title={"Completed"} total={String(dashboardDataMonthly?.Monthly_Booking_Status_Count?.completed || 0)} rate={""}  >
-          <Calendar />
-        </CardDataStats>
-        <CardDataStats title={"failed"} total={String(dashboardDataMonthly?.Monthly_Booking_Status_Count?.failed || 0)} rate={""}  >
-          <Calendar />
-        </CardDataStats>
-        <CardDataStats title={"pending"} total={String(dashboardDataMonthly?.Monthly_Booking_Status_Count?.pending || 0)} rate={""}  >
-          <Calendar />
-        </CardDataStats>
+        {Object.entries(dashboardDataMonthly).map(([status, value] :any, key) => (
+          <Card key={key} title={status}data={value} > <Calendar /></Card>      
+      ))}
       </div>
-
     </>
   );
 };
--- src/components/dashboardCards/Card.tsx
+++ src/components/dashboardCards/Card.tsx
@@ -1,5 +1,4 @@
 import React, { ReactNode } from "react";
-
 interface CardDataStatsProps {
   title: string;
   data: any;
@@ -12,9 +11,8 @@ const Card: React.FC<CardDataStatsProps> = ({
   children
 
 }) => {
-  console.log("datadatadatadata",data)
   return (
-    <div  className="rounded-md border border-stroke bg-white px-7.5 py-6 shadow-default ">
+    <div className="rounded-md border border-stroke bg-white px-7.5 py-6 shadow-default ">
       <div className="flex h-11.5 w-11.5 items-center justify-center rounded-full bg-blue-400 text-white">
         {children}
       </div>
@@ -24,13 +22,24 @@ const Card: React.FC<CardDataStatsProps> = ({
           <h4 className="text-title-md font-bold text-black ">
             {title?.replace(/_/g, ' ')}
           </h4>
-          {data && typeof data === 'object' ? Object.entries(data).map(([status, value] :any, key) => (
-                  <span key={key} className="text-sm font-medium text-black text-red-500"><br/>
+
+          {typeof data === 'object' ? Object.entries(data).map(([status, value]: any, key) => (
+            <span key={key} style={{
+              fontWeight: "bold",
+              color: status == "Completed" ?
+                "green" : status == "Pending" ?
+                  "orange" : status == "Failed" ? "red"
+                    : '#5762ff'
+            }}
+              className="text-sm font-medium text-black text-red-500">
+              <br />
               {status?.replace(/_/g, ' ')} - {value}
             </span>
-          )) : <span  className="text-sm font-medium text-black text-red-500"><br/>
-          {title?.replace(/_/g, ' ')} - {data}
-        </span>}
+          )) : <span className="text-sm font-medium text-black text-red-500"><br />
+            {console.log("datadatadatadatadata", data)}
+            {title?.replace(/_/g, ' ')} - {data}
+          </span>}
+          
         </div>
 
       </div>
