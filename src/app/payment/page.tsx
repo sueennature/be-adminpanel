0d859439b79dcf101049c7ce6b61e2da814d93a8
Yasiru-Deshan <yasirudeshan3@gmail.com>
2024-12-12 00:49:24 +0530

    added payment view and removed admin from sidebar

--- src/app/payment/page.tsx
+++ src/app/payment/page.tsx
@@ -3,6 +3,7 @@ import { Metadata } from "next";
 import DefaultLayout from "@/components/Layouts/DefaultLayout";
 import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
 import CheckAvailability from '@/components/Discounts/CheckAvailability'
+import ViewPayments from "@/components/Payments/page";
 // import BookingTable from "@/components/bookings";
 
 export const metadata: Metadata = {
@@ -15,7 +16,7 @@ const PaymentPage = () => {
   return (
     <DefaultLayout>
         <Breadcrumb pageName="payment" firstLink={"/payment"}/>
-      <>The Content Not Found</>
+      <ViewPayments/>
     </DefaultLayout>
   );
 };
